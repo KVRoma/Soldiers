e558337f4def2ec50c8bf915b9ec178519e8492e
Roman Kuchinik <kvroma83@gmail.com>
2020-05-21 00:09:47 +0300

    Add project files.

---
+++ Soldiers/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Soldiers.ViewModel;
+using Soldiers.View;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Soldiers
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public DisplayRootRegistry displayRootRegistry = new DisplayRootRegistry();
+        
+        public App()
+        {
+            displayRootRegistry.RegisterWindowType<MainViewModel, MainView>();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainViewModel = new MainViewModel();
+            displayRootRegistry.ShowPresentation(mainViewModel);
+
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            displayRootRegistry.UnregisterWindowType<MainViewModel>();
+        }
+    }
+}
---
+++ Soldiers/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soldiers.ViewModel
+{
+    public class MainViewModel : ViewModel
+    {
+        public string NameWindow { get; } = "Soldiers - 2020";        
+    }
+}
